ccd12b3ff110f4f11e3f429d3b96f432df8f135e
GaMbLeR08 <109575459+GaMbLeR08@users.noreply.github.com>
2022-11-04 11:58:30 +0530

    commited

---
+++ New.java
@@ -0,0 +1,7 @@
+class New {
+  public static void main(String[] args) {
+
+            int sum = x + y;
+            System.out.println(sum); 
+  }
+}
---
+++ SumOfNumber.java
@@ -0,0 +1,15 @@
+
+public class SumOfNumber 
+{  
+public static void main(String args[])  
+{  
+x=10;
+ y=10;
+sum = x+y;
+    
+System.out.println("X:" +x); 
+System.out.println("Y: " +y); 
+System.out.println("The sum of two numbers x and y is: " + sum);  
+}  
+}
+
